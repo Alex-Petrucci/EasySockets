e2f4af4021cd7566777f729e033188342ac095a3
Alex-Petrucci <alexleighpetrucci@gmail.com>
2025-12-03 18:32:34 +1100

    Updated EasySockets.hpp to be more specific with platform macros

--- include/EasySockets/EasySockets.hpp
+++ include/EasySockets/EasySockets.hpp
@@ -3,10 +3,27 @@
 #include "Api/SocketApi.hpp"
 #include "Api/ContextApi.hpp"
 
-#ifdef _WIN32
+#if defined(_WIN32)
 
-#include "Windows/WindowsSocket.hpp"
-#include "Windows/WinsockContext.hpp"
+    #include "Windows/WindowsSocket.hpp"
+    #include "Windows/WinsockContext.hpp"
+
+    #define SOCKET_TYPE WindowsSocket
+    #define CONTEXT_TYPE WinsockContext
+
+#elif defined(__unix__) || defined(__unix) || defined(__APPLE__)
+
+    #include "Posix/PosixSocket.hpp"
+    #include "Posix/PosixContext.hpp"
+
+    #define SOCKET_TYPE PosixSocket
+    #define CONTEXT_TYPE PosixContext
+
+#else
+
+    #error EasySockets does not support this platform
+
+#endif
 
 namespace es
 {
@@ -14,15 +31,15 @@ namespace es
     /**
      * An object which represents a connection between two applications.
      * <br>On creation, type of ip addresses that the socket will use and the
-     * layer 4/transport layer protocol is specified. 
-     * <br>From this, methods like <code>.bind_to(EndPoint end_point)</code> 
-     * and <code>.connect_to(EndPoint end_point)</code> to give the socket 
+     * layer 4/transport layer protocol is specified.
+     * <br>From this, methods like <code>.bind_to(EndPoint end_point)</code>
+     * and <code>.connect_to(EndPoint end_point)</code> to give the socket
      * an address and port so that it can start communicating with other applications.
      * <br>For connectionless protocols like UDP, data can be sent and received
      * without binding or connecting first.
      * <br>Can be moved from but not copied.
      */
-    using Socket = WindowsSocket;
+    using Socket = SOCKET_TYPE;
 
     ES_API_DOC(context)
     /**
@@ -36,30 +53,10 @@ namespace es
      * </code>
      * <br>Can be moved from but not copied.
      */
-    using Context = WinsockContext;
+    using Context = CONTEXT_TYPE;
 }
-
-#elif __unix__
-
-#include "Posix/PosixSocket.hpp"
-#include "Posix/PosixContext.hpp"
-
-namespace es
-{
-    ES_API_DOC(socket)
-    using Socket = PosixSocket;
-
-    ES_API_DOC(context)
-    using Context = PosixContext;
-}
-
-#else // !(_WIN32 || __unix__)
-
-#error EasySockets does not support this platform
-
-#endif // _WIN32/__unix__
 
 static_assert(es::SocketApi<es::Socket>,
-    "Socket API for current platform is invalid. This problem is the library's fault, not the end user's.");
+              "Socket API for current platform is invalid. This problem is the library's fault, not the end user's.");
 static_assert(es::ContextApi<es::Context>,
-    "Context API for current platform is invalid. This problem is the library's fault, not the end user's.");
+              "Context API for current platform is invalid. This problem is the library's fault, not the end user's.");
